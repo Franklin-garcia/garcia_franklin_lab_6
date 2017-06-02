3c7a6596c4a077c5563d3e388976d42f269d162e
franklin garcia <franklin__garcia@unitec.edu>
2017-06-02 13:34:49 -0600

    clases

--- lab6_garcia_franklin/src/lab6_garcia_franklin/Clientes.java
+++ lab6_garcia_franklin/src/lab6_garcia_franklin/Clientes.java
@@ -5,10 +5,52 @@
  */
 package lab6_garcia_franklin;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Franklin Garcia
  */
 public class Clientes {
+    private int edad;
+    private String nombre;
+    private ArrayList<Productos>listaProductos=new ArrayList();
+
+    public Clientes() {
+    }
+
+    public Clientes(int edad, String nombre) {
+        this.edad = edad;
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Productos> getListaProductos() {
+        return listaProductos;
+    }
+
+    public void setListaProductos(ArrayList<Productos> listaProductos) {
+        this.listaProductos = listaProductos;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
     
 }
